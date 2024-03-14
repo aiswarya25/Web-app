bb649fd8c1301e66d0543bcde3052d4450b0fc6a
aiswarya25 <aiswaryapbindhu3@gmail.com>
2024-03-14 17:20:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+
+const PORT =   process.env.PORT || 8080
+
+
+
+
+   
+app.listen(PORT, ()=>{
+     console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+
